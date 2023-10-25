f879d9b881130e81b0118c9d3db223e079b06c74
zamoro07 <148032786+zamoro07@users.noreply.github.com>
2023-10-25 05:04:32 +0100

    Create Person.java

---
+++ MIDTERM/Person.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Person {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int age;
+    private String birthday;
+    private String address;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getMiddleName() {
+        return middleName;
+    }
+    
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getBirthday() {
+        return birthday;
+    }
+    
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        Person person = new Person();
+        person.setFirstName("Jana Alliah");
+        person.setMiddleName("Gabutan");
+        person.setLastName("Zamoro");
+        person.setAge(20);
+        person.setBirthday("July 24, 2003");
+        person.setAddress("J.Labra St. Guadalupe Cebu City");
+
+        System.out.println("First Name: " + person.getFirstName());
+        System.out.println("Middle Name: " + person.getMiddleName());
+        System.out.println("Last Name: " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Birthday: " + person.getBirthday());
+        System.out.println("Address: " + person.getAddress());
+    }
+}
